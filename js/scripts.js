4e570731c63956209c00a5feeb29a2c80c53b660
Joe Jordan <lankyjoe@gmail.com>
2021-03-18 14:51:54 -0700

    Added pokemon
    Added first 18 pokemon to pokemonList variable as an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-alert('Hello world');
-var favoriteFood = 'TACOS!';
-document.write(favoriteFood);
+// array of pokemon
+let pokemonList = [
+  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"]},
+  { name: "Ivysaur", height: 1, types: ["grass", "poison"]},
+  { name: "Venusaur", height: 2, types: ["grass", "poison"]},
+  { name: "Charmander", height: 0.6, types: ["fire"]},
+  { name: "Charmeleon", height: 1.1, types: ["fire"]},
+  { name: "Charizard", height: 1.7, types: ["fire", "flying"]},
+  { name: "Squirtle", height: 0.5, types: ["water"]},
+  { name: "Wartortle", height: 1, types: ["water"]},
+  { name: "Blastoise", height: 1.6, types: ["water"]},
+  { name: "Caterpie", height: 0.3, types: ["bug"]},
+  { name: "Metapod", height: 0.7, types: ["bug"]},
+  { name: "Butterfree", height: 1.1, types: ["bug", "flying"]},
+  { name: "Weedle", height: 0.3, types: ["bug", "poison"]},
+  { name: "Kakuna", height: 0.6, types: ["bug", "poison"]},
+  { name: "Beedrill", height: 1, types: ["bug", "poison"]},
+  { name: "Pidgey", height: 0.3, types: ["flying", "normal"]},
+  { name: "Pidgeotto", height: 1.1, types: ["flying", "normal"]},
+  { name: "Pidgeot", height: 1.5, types: ["flying", "normal"]}
+];
