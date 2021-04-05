5278455dc98052a257a2b4377ab7a2734434b7a4
Joe Jordan <lankyjoe@gmail.com>
2021-04-05 12:29:51 -0700

    Update scripts.js
    Added showDetails function to iffy returns

--- js/scripts.js
+++ js/scripts.js
@@ -59,7 +59,8 @@ let pokemonRepository = (function () {
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
