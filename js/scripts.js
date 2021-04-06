5a77c030f9772a00e1e1914c5c7fd6591d9e0996
Joe Jordan <lankyjoe@gmail.com>
2021-04-05 17:24:25 -0700

    Polyfills and fetch functions
    Added promise and fetch polyfill scripts; added loadList and loadDetails fetch functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,8 @@
 
 let pokemonRepository = (function () {
 
-// array of pokemon
-
-  let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"]},
-  	{ name: "Ivysaur", height: 1, types: ["grass", "poison"]},
-	{ name: "Venusaur", height: 2, types: ["grass", "poison"]},
-	{ name: "Charmander", height: 0.6, types: ["fire"]},
-	{ name: "Charmeleon", height: 1.1, types: ["fire"]},
-	{ name: "Charizard", height: 1.7, types: ["fire", "flying"]},
-	{ name: "Squirtle", height: 0.5, types: ["water"]},
-	{ name: "Wartortle", height: 1, types: ["water"]},
-	{ name: "Blastoise", height: 1.6, types: ["water"]},
-	{ name: "Caterpie", height: 0.3, types: ["bug"]},
-	{ name: "Metapod", height: 0.7, types: ["bug"]},
-	{ name: "Butterfree", height: 1.1, types: ["bug", "flying"]},
-	{ name: "Weedle", height: 0.3, types: ["bug", "poison"]},
-	{ name: "Kakuna", height: 0.6, types: ["bug", "poison"]},
-	{ name: "Beedrill", height: 1, types: ["bug", "poison"]},
-	{ name: "Pidgey", height: 0.3, types: ["flying", "normal"]},
-	{ name: "Pidgeotto", height: 1.1, types: ["flying", "normal"]},
-	{ name: "Pidgeot", height: 1.5, types: ["flying", "normal"]}
-	];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
 // Below allows pokemon to be added to the repository
 
@@ -47,9 +27,11 @@ let pokemonRepository = (function () {
 
 // Below logs a pokemon's name
 
-  function showDetails(pokemon) {
-	console.log(pokemon);
-  }
+function showDetails(pokemon) {
+  loadDetails(pokemon).then(function () {
+    console.log(pokemon);
+  });
+}
 
   function addListItem(pokemon){
     let pokemonList = document.querySelector('.pokemon-list');
@@ -64,18 +46,74 @@ let pokemonRepository = (function () {
     });
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
-    showDetails: showDetails
+    showDetails: showDetails,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
-// below is the forEach loop
-
-pokemonRepository.getAll().forEach(function(pokemon) {
-
-  pokemonRepository.addListItem(pokemon);
-
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
 });
+
+/* original array of pokemon
+
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"]},
+  	{ name: "Ivysaur", height: 1, types: ["grass", "poison"]},
+	{ name: "Venusaur", height: 2, types: ["grass", "poison"]},
+	{ name: "Charmander", height: 0.6, types: ["fire"]},
+	{ name: "Charmeleon", height: 1.1, types: ["fire"]},
+	{ name: "Charizard", height: 1.7, types: ["fire", "flying"]},
+	{ name: "Squirtle", height: 0.5, types: ["water"]},
+	{ name: "Wartortle", height: 1, types: ["water"]},
+	{ name: "Blastoise", height: 1.6, types: ["water"]},
+	{ name: "Caterpie", height: 0.3, types: ["bug"]},
+	{ name: "Metapod", height: 0.7, types: ["bug"]},
+	{ name: "Butterfree", height: 1.1, types: ["bug", "flying"]},
+	{ name: "Weedle", height: 0.3, types: ["bug", "poison"]},
+	{ name: "Kakuna", height: 0.6, types: ["bug", "poison"]},
+	{ name: "Beedrill", height: 1, types: ["bug", "poison"]},
+	{ name: "Pidgey", height: 0.3, types: ["flying", "normal"]},
+	{ name: "Pidgeotto", height: 1.1, types: ["flying", "normal"]},
+	{ name: "Pidgeot", height: 1.5, types: ["flying", "normal"]}
+	];
+
+*/
