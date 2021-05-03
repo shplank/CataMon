ead5409b1cc90c7f8a2d95504ec14ac9da6d076d
Joe Jordan <lankyjoe@gmail.com>
2021-05-03 13:35:57 -0700

    Bootstrap
    Added Bootstrap into html and added weight to modal items

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ let pokemonRepository = (function(){
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+  let loading = document.querySelector('#loading-icon');
+
 // below allows pokemon to be added to the repository
 
   function add(pokemon) {
@@ -46,7 +48,8 @@ let pokemonRepository = (function(){
     }).then(function (details) {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
-      item.types = details.types;
+      item.weight = details.weight;
+      item.types = [...details.types];
     }).catch(function (e) {
       console.error(e);
     });
@@ -65,6 +68,16 @@ let pokemonRepository = (function(){
     button.addEventListener('click', function(event) {
       showDetails(pokemon);
     });
+  }
+
+// below creates a loading message
+
+  function showLoadingMessage() {
+    loading.appendChild('Loading');
+  }
+
+  function hideLoadingMessage() {
+
   }
 
 // below creates a modal with pokemon info
@@ -91,16 +104,28 @@ function showModal(pokemon) {
     let nameElement = document.createElement('h1');
     nameElement.innerText = pokemon.name;
 
-    let heightElement = document.createElement('p');
-    heightElement.innerText = "Height: " + pokemon.height;
-
     let imageElement = document.createElement('img');
     imageElement.src = pokemon.imageUrl;
 
+    let heightElement = document.createElement('p');
+    heightElement.innerText = "Height: " + (pokemon.height / 10) + " m";
+
+    let weightElement = document.createElement('p');
+    weightElement.innerText = "Weight: " + (pokemon.weight / 10) + " kg";
+
+    let typesElement = document.createElement('p');
+    let types = 'Type(s): ';
+    pokemon.types.forEach(function(item) {
+      types += '<p>' + item.type.name + '</p>';
+    });
+    typesElement.innerHTML = types;
+    
     modal.appendChild(closeButtonElement);
     modal.appendChild(nameElement);
-    modal.appendChild(heightElement);
     modal.appendChild(imageElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(weightElement);
+    modal.appendChild(typesElement);
     modalContainer.appendChild(modal);
 
 	modalContainer.classList.add('is-visible');
